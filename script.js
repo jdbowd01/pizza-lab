b230dae9f647cf3e24e87458d2aaf8dee9f3eda0
Unknown <CHawley@student.neumont.edu>
2018-05-27 21:45:06 -0600

    Customize Panel finished

--- script.js
+++ script.js
@@ -16,8 +16,25 @@ function prebuiltTab(evt) {
 function customizeTab(evt) {
   document.getElementById("tabTitle").children[0].innerHTML = "Customize Pizza";
   document.getElementById("prebuiltHolder").style.display = "none";
+  document.getElementById("customHolder").style.display = "flex";
   document.getElementById("orderBtn").style.display = "flex";
   document.getElementById("orderBtn").style.marginLeft = "auto";    
   document.getElementById("checkoutBtn").style.marginLeft = "2%";  
-  document.getElementsByClassName("pizzaTab")[1].children.style.display = "flex";
+  document.getElementsByClassName("pizzaTab")[1].children[0].style.display = "flex";
+  FillPizza();
+}
+function FillPizza(){
+  var pizza = document.getElementById('pizzaDrawing');
+  var crust = new Image(250, 250);
+  crust.src = 'images/crusts/regular.png';
+  crust.style="z-index:2; position:absolute; top:0px; left:0px;";
+  var sauce = new Image(225, 225);
+  sauce.src = 'images/sauces/normal.png';
+  sauce.style="z-index:3; position:absolute; top:12.5px; left:12.5px;";
+  var cheese = new Image(225, 225);
+  cheese.src = 'images/cheeses/normal.png';
+  cheese.style="z-index:4; position:absolute; top:12.5px; left:12.5px;";
+  pizza.appendChild(crust);
+  pizza.appendChild(sauce);
+  pizza.appendChild(cheese);
 }
